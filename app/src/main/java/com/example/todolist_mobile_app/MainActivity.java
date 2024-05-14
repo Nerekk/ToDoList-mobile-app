7b4ad0f87e84f0246357a0c728a1b6649ec8c8ba
Nerekk <arkadiusz.salaj.02@gmail.com>
2024-05-14 19:29:01 +0200

    RecyclerView refactoring

--- app/src/main/java/com/example/todolist_mobile_app/MainActivity.java
+++ app/src/main/java/com/example/todolist_mobile_app/MainActivity.java
@@ -10,6 +10,7 @@ import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.todolist_mobile_app.Recycler.RecyclerViewManager;
 import com.example.todolist_mobile_app.Recycler.TaskData;
 import com.example.todolist_mobile_app.Recycler.TaskListAdapter;
 
@@ -18,30 +19,14 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    List<TaskData> tasks;
-    RecyclerView recyclerView;
-    TaskListAdapter adapter;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        tasks = getExampleData();
-        recyclerView = findViewById(R.id.recyclerView);
-        adapter = new TaskListAdapter(tasks, getApplication());
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(adapter);
+        new RecyclerViewManager(this);
     }
 
-    private List<TaskData> getExampleData() {
-        List<TaskData> data = new ArrayList<>();
-        data.add(new TaskData("Test1", "Description", false, Category.Home));
-        data.add(new TaskData("TESTUJEMY", "Description 1245521", true, Category.Recreation));
-        data.add(new TaskData("DLUGI DLUGI TEKST", "Descrip", false, Category.Other));
-        data.add(new TaskData("Zadanie", "Description dapsodkap", true, Category.Health));
 
-        return data;
-    }
 }
---
+++ app/src/main/java/com/example/todolist_mobile_app/Recycler/RecyclerViewManager.java
@@ -0,0 +1,40 @@
+package com.example.todolist_mobile_app.Recycler;
+
+import android.content.Context;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.todolist_mobile_app.Category;
+import com.example.todolist_mobile_app.MainActivity;
+import com.example.todolist_mobile_app.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RecyclerViewManager {
+    List<TaskData> tasks;
+    RecyclerView recyclerView;
+    TaskListAdapter adapter;
+    MainActivity activity;
+
+    public RecyclerViewManager(MainActivity activity) {
+        this.activity = activity;
+
+        tasks = getExampleData();
+        recyclerView = activity.findViewById(R.id.recyclerView);
+        adapter = new TaskListAdapter(tasks, activity.getApplication());
+        recyclerView.setLayoutManager(new LinearLayoutManager(activity));
+        recyclerView.setAdapter(adapter);
+    }
+
+    private List<TaskData> getExampleData() {
+        List<TaskData> data = new ArrayList<>();
+        data.add(new TaskData("Test1", "Description", false, Category.Home));
+        data.add(new TaskData("TESTUJEMY", "Description 1245521", true, Category.Recreation));
+        data.add(new TaskData("DLUGI DLUGI TEKST", "Descrip", false, Category.Other));
+        data.add(new TaskData("Zadanie", "Description dapsodkap", true, Category.Health));
+
+        return data;
+    }
+}
