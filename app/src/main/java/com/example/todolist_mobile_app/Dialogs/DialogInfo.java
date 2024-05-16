ac34da92d333413dd9c610e4dc36ba59402faaf6
Nerekk <arkadiusz.salaj.02@gmail.com>
2024-05-16 21:18:14 +0200

    Listener implementation for editing task activity

--- app/src/main/java/com/example/todolist_mobile_app/Dialogs/DialogInfo.java
+++ app/src/main/java/com/example/todolist_mobile_app/Dialogs/DialogInfo.java
@@ -2,26 +2,34 @@ package com.example.todolist_mobile_app.Dialogs;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 
+import com.example.todolist_mobile_app.AddingActivity.AddTaskActivity;
 import com.example.todolist_mobile_app.R;
 import com.example.todolist_mobile_app.Data.TaskData;
 import com.example.todolist_mobile_app.Recycler.TaskListAdapter;
+import com.example.todolist_mobile_app.Utils.DateFormatter;
 
 public class DialogInfo extends Dialog {
     private TaskData task;
     private TaskListAdapter adapter;
+    private Context context;
+    ImageView ivEdit;
 
     public DialogInfo(@NonNull Context context, TaskListAdapter adapter, TaskData task) {
         super(context);
         this.task = task;
         this.adapter = adapter;
+        this.context = context;
         prepareDialog();
+        setListeners();
         show();
     }
 
@@ -34,7 +42,18 @@ public class DialogInfo extends Dialog {
         ((TextView)findViewById(R.id.dialogDesc)).setText(task.getDescription());
         ((TextView)findViewById(R.id.dialogCategory)).setText(task.getCategory().toString());
         ((TextView)findViewById(R.id.dialogNotifs)).setText(task.getNotificationStatus());
-        ((TextView)findViewById(R.id.dialogEnd)).setText(task.getEndTimeFormatted());
-        ((TextView)findViewById(R.id.dialogStart)).setText(task.getStartTimeFormatted());
+        ((TextView)findViewById(R.id.dialogEnd)).setText(DateFormatter.getFullToString(task.getEndTime()));
+        ((TextView)findViewById(R.id.dialogStart)).setText(DateFormatter.getFullToString(task.getStartTime()));
     }
+
+    private void setListeners() {
+        ivEdit = findViewById(R.id.ivEdit);
+        ivEdit.setOnClickListener(view -> {
+            Intent intent = new Intent(context, AddTaskActivity.class);
+            intent.putExtra(TaskData.ID, task.getId());
+            dismiss();
+            context.startActivity(intent);
+        });
+    }
+
 }
