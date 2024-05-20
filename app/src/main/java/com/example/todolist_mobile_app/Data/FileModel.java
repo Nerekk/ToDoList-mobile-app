00c351505202cbf1b654b2bd9e33638f2adecea7
Nerekk <arkadiusz.salaj.02@gmail.com>
2024-05-20 14:22:48 +0200

    FileModel class implementation to store file important data

---
+++ app/src/main/java/com/example/todolist_mobile_app/Data/FileModel.java
@@ -0,0 +1,27 @@
+package com.example.todolist_mobile_app.Data;
+
+public class FileModel {
+    private String fileName;
+    private String filePath;
+
+    public FileModel(String fileName, String filePath) {
+        this.fileName = fileName;
+        this.filePath = filePath;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+}
