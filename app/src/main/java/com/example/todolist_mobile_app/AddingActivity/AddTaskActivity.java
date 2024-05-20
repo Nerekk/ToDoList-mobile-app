8e2eed163561c1dd9a472d689342136ca00ef200
Nerekk <arkadiusz.salaj.02@gmail.com>
2024-05-20 14:17:51 +0200

    Optimizing imports in AddTaskActivity

--- app/src/main/java/com/example/todolist_mobile_app/AddingActivity/AddTaskActivity.java
+++ app/src/main/java/com/example/todolist_mobile_app/AddingActivity/AddTaskActivity.java
@@ -1,42 +1,16 @@
 package com.example.todolist_mobile_app.AddingActivity;
 
-import android.app.AlertDialog;
-import android.app.DatePickerDialog;
-import android.app.TimePickerDialog;
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.DatePicker;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.Spinner;
-import android.widget.TimePicker;
-import android.widget.Toast;
-import android.widget.ToggleButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.todolist_mobile_app.Enums.Categories;
 import com.example.todolist_mobile_app.Database.DatabaseManager;
-import com.example.todolist_mobile_app.Database.TaskDatabase;
-import com.example.todolist_mobile_app.Enums.Notifications;
 import com.example.todolist_mobile_app.R;
-import com.example.todolist_mobile_app.Data.TaskData;
-import com.example.todolist_mobile_app.Utils.DateFormatter;
-import com.google.android.material.button.MaterialButtonToggleGroup;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Calendar;
-import java.util.Locale;
 
 public class AddTaskActivity extends AppCompatActivity {
-
     private AddTaskViewManager addTaskViewManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,11 +20,4 @@ public class AddTaskActivity extends AppCompatActivity {
         DatabaseManager.initDatabase(this);
         addTaskViewManager = new AddTaskViewManager(this);
     }
-
-
-
-
-
-
-
 }
